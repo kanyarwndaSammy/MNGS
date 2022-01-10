7663ea7b8c7af8113c1b6b8faf2d833bf9ca8edd
kanyarwndaSammy <94258882+kanyarwndaSammy@users.noreply.github.com>
2022-01-10 02:26:32 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 error_reporting(0); 
-include('application/includes/config.php');
+include('includes/config.php');
 if (isset($_REQUEST['login'])) {
     $username=$_REQUEST['username'];
     $password=md5($_REQUEST['password']);
@@ -14,10 +14,10 @@ if (isset($_REQUEST['login'])) {
     if($username=="Admin"){
 
     
-    echo "<script>alert('Welcome Super Admin');window.location.replace('application/dashboard.php')</script>";
+    echo "<script>alert('Welcome Super Admin');window.location.replace('dashboard.php')</script>";
 }
    elseif($username=="Other"){
-    echo "<script>alert('Employee Enjoy To use!!!');window.location.replace('users/dayin.php')</script>";
+    echo "<script>alert('Employee Enjoy To use!!!');window.location.replace('dayin.php')</script>";
 
 }
 else{
@@ -35,12 +35,12 @@ else{
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>MNGS | Signin Page</title>
-        <link rel="stylesheet" href="application/css/bootstrap.min.css" media="screen" >
-        <link rel="stylesheet" href="application/css/font-awesome.min.css" media="screen" >
-        <link rel="stylesheet" href="application/css/animate-css/animate.min.css" media="screen" >
-        <link rel="stylesheet" href="application/css/prism/prism.css" media="screen" > <!-- USED FOR DEMO HELP - YOU CAN REMOVE IT -->
-        <link rel="stylesheet" href="application/css/main.css" media="screen" >
-        <script src="application/js/modernizr/modernizr.min.js"></script>
+        <link rel="stylesheet" href="css/bootstrap.min.css" media="screen" >
+        <link rel="stylesheet" href="css/font-awesome.min.css" media="screen" >
+        <link rel="stylesheet" href="css/animate-css/animate.min.css" media="screen" >
+        <link rel="stylesheet" href="css/prism/prism.css" media="screen" > <!-- USED FOR DEMO HELP - YOU CAN REMOVE IT -->
+        <link rel="stylesheet" href="css/main.css" media="screen" >
+        <script src="js/modernizr/modernizr.min.js"></script>
     </head>
     <body style="background:#4bd786;">
         <div class="container">
@@ -106,17 +106,17 @@ else{
                 <!-- /.row -->
 
         <!-- ========== COMMON JS FILES ========== -->
-        <script src="application/js/jquery/jquery-2.2.4.min.js"></script>
-        <script src="application/js/jquery-ui/jquery-ui.min.js"></script>
-        <script src="application/js/bootstrap/bootstrap.min.js"></script>
-        <script src="application/js/pace/pace.min.js"></script>
-        <script src="application/js/lobipanel/lobipanel.min.js"></script>
-        <script src="application/js/iscroll/iscroll.js"></script>
+        <script src="js/jquery/jquery-2.2.4.min.js"></script>
+        <script src="js/jquery-ui/jquery-ui.min.js"></script>
+        <script src="js/bootstrap/bootstrap.min.js"></script>
+        <script src="js/pace/pace.min.js"></script>
+        <script src="js/lobipanel/lobipanel.min.js"></script>
+        <script src="js/iscroll/iscroll.js"></script>
 
         <!-- ========== PAGE JS FILES ========== -->
 
         <!-- ========== THEME JS ========== -->
-        <script src="application/js/main.js"></script>
+        <script src="js/main.js"></script>
         <script>
             $(function(){
 
